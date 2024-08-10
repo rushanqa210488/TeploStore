bbefc3e214deff34da57a1a30e4d832eb2f39f4d
Rushan <rushan21@bk.ru>
2024-08-10 15:03:21 +0300

    Deleted description frfrom product-card

--- products/models.py
+++ products/models.py
@@ -13,7 +13,7 @@ class ProductCategory(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)

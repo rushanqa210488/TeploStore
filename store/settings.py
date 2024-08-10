6e62019a1cefaadb8870267afa095ef024c7721d
Rushan <rushan21@bk.ru>
2024-08-10 15:19:02 +0300

    Added STATIC_ROOT

--- store/settings.py
+++ store/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (BASE_DIR / 'static',)
+# STATICFILES_DIRS = (BASE_DIR / 'static',)
 STATIC_ROOT = 'static/'
 
 MEDIA_URL = '/media/'

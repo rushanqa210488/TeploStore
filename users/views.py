30b70d13a65cec7fc20a5aafffebd1e6aeeb1f89
Rushan <rushan21@bk.ru>
2024-08-11 18:41:07 +0300

    Created login, logout, registration functions

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponsePermanentRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 
 from users.models import User
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 
 
 def login(request):
@@ -27,8 +27,26 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Поздравляем! Вы успешно зарегистрированы!')
             return HttpResponsePermanentRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context=context)
+
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponsePermanentRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context=context)
+
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponsePermanentRedirect(reverse('index'))
